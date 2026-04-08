b4cbc17c982604ac4826ea47ae11146cd4d7f092
Muskan Gautam <muskangautam7064@gmail.com>
2026-04-08 16:22:04 +0530

    update

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -4,6 +4,7 @@
  */
 
 import * as userRepo from "../repos/UserRepo.js";
+import * as accountRepo from "../repos/AccountRepo.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 export const getUsers = asyncHandler(async (req, res) => {
@@ -43,6 +44,27 @@ export const deleteUser = asyncHandler(async function (req, res) {
 export const deleteByEmail = asyncHandler(async function (req, res) {
   await userRepo.deleteByEmail(req.query.email);
   return res.sendStatus(200);
+});
+
+export const getUserProfile = asyncHandler(async function (req, res) {
+  const { id } = req.params;
+
+  const user = await userRepo.getById(id);
+  if (!user) throw new Error("User not found");
+
+  const account = await accountRepo.getAccountByUserId(id);
+  if (!account) throw new Error("Account not found");
+
+  // For now, accountLifecycle is null, friendRequest and chat are null
+  const profile = {
+    user: user.toJSON(),
+    account: account.toJSON(),
+    accountLifecycle: null,
+    friendRequest: null,
+    chat: null,
+  };
+
+  return res.status(200).json(profile);
 });
 
 
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,10 +3,13 @@
  */
 import express from "express";
 import * as userController from "../controllers/UserController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", userController.getUsers);
+
+router.get("/:id/profile", verifyToken, userController.getUserProfile);
 
 router.delete("/", userController.deleteByEmail);
 
